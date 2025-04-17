25998becf7e4f58f6f3cb8eb0270a023e4eb31eb
Nirav Das <nirdas6268@gmail.com>
2025-04-17 13:29:14 +0530

    Initial commit: project structure and setup

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
